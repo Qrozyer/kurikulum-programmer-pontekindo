f1b72da065764e377c34708711c681df0966b94a
Qrozyer <10huruf@gmail.com>
2023-07-18 13:11:46 +0700

    tebak

--- sprint1/Tugas/tebak.php
+++ sprint1/Tugas/tebak.php
@@ -1,85 +1,34 @@
 <?php
 
-// // Fungsi untuk mengambil input dari pengguna melalui stdin
-// function getInput()
-// {
-//     return trim(fgets(STDIN));
-// }
-
-// // Fungsi untuk memeriksa apakah tebakan pengguna benar atau salah
-// function checkGuess($number, $guess)
-// {
-//     if ($guess < $number) {
-//         echo "Terlalu rendah! Coba lagi.\n";
-//     } elseif ($guess > $number) {
-//         echo "Terlalu tinggi! Coba lagi.\n";
-//     } else {
-//         echo "Tebakanmu benar! Angka yang kamu tebak adalah $number.\n";
-//         exit();
-//     }
-// }
-
-// // Generate angka acak antara 1 dan 100
-// $number = mt_rand(1, 100);
-
-// echo "Selamat datang di permainan Tebak Angka!\n";
-// echo "Saya sudah memilih sebuah angka antara 1 dan 100. Coba tebak ya!\n";
-
-// while (true) {
-//     echo "Tebakanmu: ";
-//     $guess = getInput();
-
-//     if (is_numeric($guess)) {
-//         checkGuess($number, $guess);
-//     } else {
-//         echo "Masukkan angka yang valid.\n";
-//     }
-// }
-
-
-// Fungsi untuk mengambil input dari pengguna melalui stdin
-function getInput()
-{
-    return trim(fgets(STDIN));
-}
-
-// Fungsi untuk memeriksa apakah tebakan pengguna benar atau salah
-function checkGuess($number, $guess, $attempt)
-{
-    if ($guess < $number) {
-        echo "Terlalu rendah! Coba lagi.\n";
-    } elseif ($guess > $number) {
-        echo "Terlalu tinggi! Coba lagi.\n";
-    } else {
-        echo "Tebakanmu benar! Angka yang kamu tebak adalah $number.\n";
-        exit();
-    }
-
-    if ($attempt == 3) {
-        echo "Kesempatanmu habis. Angka yang benar adalah $number.\n";
-        exit();
-    }
-}
-
-// Generate angka acak antara 1 dan 10
-$number = mt_rand(1, 10);
+$angka = mt_rand(1, 9);
 
 echo "Selamat datang di permainan Tebak Angka!\n";
-echo "Saya sudah memilih sebuah angka antara 1 dan 100. Coba tebak ya!\n";
+echo "Tebak sebuah angka antara 1 dan 9. Anda memiliki 3 kali kesempatan!\n";
 
-$attempt = 1;
-while ($attempt <= 3) {
-    echo "Tebakanmu (percobaan ke-$attempt): ";
-    $guess = getInput();
+$nyawa = 1;
+while ($nyawa <= 3) {
+    echo "Tebakanmu (percobaan ke-$nyawa): ";
+    $tebak = trim(fgets(STDIN));
 
-    if (is_numeric($guess)) {
-        checkGuess($number, $guess, $attempt);
-        $attempt++;
+    if (is_numeric($tebak)) {
+        if ($tebak < $angka) {
+                echo "Terlalu rendah! Coba lagi.\n";
+            } elseif ($tebak > $angka) {
+                echo "Terlalu tinggi! Coba lagi.\n";
+            } else {
+                echo "Tebakanmu benar! Angka yang kamu tebak adalah $angka.\n";
+                exit();
+        }
+
+        if ($nyawa == 3) {
+            echo "Kesempatanmu habis. Angka yang benar adalah $angka.\n";
+            exit();
+        }
+
+        $nyawa++;
     } else {
         echo "Masukkan angka yang valid.\n";
     }
 }
 
 ?>
-
-Anda dapat menjalankan kode tersebut melalui command line dan memiliki 3 kali kesempatan untuk menebak angka.
