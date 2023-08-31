d9f3c13368d564d0853cf96a06deb239a2fd3496
Qrozyer <10huruf@gmail.com>
2023-08-31 09:39:53 +0700

    Sprint8

---
+++ Sprint8/Materi/View/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once "../Controller/class.php";
+use Data\{Laptop, Produk};
+use function Data\SayHi;
+use const Data\AUTHOR;
+
+$produk = new Produk();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
